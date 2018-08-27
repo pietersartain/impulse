da4062b99d34162be5184db4a8ad142b98008338
Pieter Sartain <pesartain@googlemail.com>
2018-08-27 19:56:05 +0100

    Allows scraping of more than one gallery

--- make.py
+++ make.py
@@ -70,9 +70,6 @@ def parse_quotes_as_json():
 #
 # We want a nice big image, so aiming for a "k" (2048 on biggest side) size image. 
 def get_image_info_as_json(api_key, gallery_id):
-    print("Gathering gallery information ...")
-
-    gallery_names = []
     images = []
 
     url = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key="+api_key+"&gallery_id="+gallery_id+"&format=json&nojsoncallback=1&extras=owner_name"
@@ -91,7 +88,7 @@ def get_image_info_as_json(api_key, gallery_id):
     image_idx = 1
 
     for image in image_list:
-        print(f"{image_idx} of {image_count}")
+        print(f"Image {image_idx} of {image_count}")
 
         info = requests.get("https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key="+api_key+"&photo_id="+image['id']+"&format=json&nojsoncallback=1")
 
@@ -103,6 +100,36 @@ def get_image_info_as_json(api_key, gallery_id):
         image_idx += 1
 
     print(" done.")
+
+    print(f"Writing images-{gallery_id}.js ...")
+
+    images = json.dumps(images)
+
+    f = open(f"static/images-{gallery_id}.js", "w")
+    f.write(images)
+    f.close()
+
+    print(" done.")
+
+    return images
+
+
+def get_all_galleries(api_key, gallery_ids):
+
+    gallery_count = len(gallery_ids)
+
+    images = []
+
+    print(f"Found {gallery_count} galleries.")
+    print("Gathering gallery information ...")
+
+    gallery_idx = 1
+
+    for gallery_id in gallery_ids:
+        print(f"Looking through {gallery_idx} of {gallery_count}")
+        images += json.loads(get_image_info_as_json(api_key, gallery_id))
+        gallery_idx += 1
+
     print("Writing images.js ...")
 
     images = json.dumps(images)
@@ -114,11 +141,12 @@ def get_image_info_as_json(api_key, gallery_id):
     print(" done.")
 
     return images
+
 #
 #
 #
 #
-def parse_template(quotes, images, greetingname, ):
+def parse_template(quotes, images, greetingname):
     print("Writing impulse.html ...")
 
     env = Environment(
@@ -138,22 +166,26 @@ def parse_template(quotes, images, greetingname, ):
 
 parser = OptionParser()
 # Defaults: action="store", type="string", 
-parser.add_option("-a", "--all", action="store_true", dest="RUN_ALL", default=False, help="Actually run everything. Without this, it's just a dry run.")
-parser.add_option("-i", action="store_true", dest="RUN_IMAGES", default=False, help="Run the image gallery scraper, to create a intermediary images.js file.")
-parser.add_option("-q", action="store_true", dest="RUN_QUOTES", default=False, help="Run the quote parser, to create a intermediary quotes.js file.")
-parser.add_option("-t", action="store_true", dest="RUN_TEMPLATE", default=False, help="Run the templating engine, to make an impulse.html file from the intermediary files.")
+parser.add_option("-a", "--all",    action="store_true", dest="RUN_ALL",      default=False, help="Actually run everything. Without this, it's just a dry run.")
+parser.add_option("-i",             action="store_true", dest="RUN_IMAGES",   default=False, help="Run the image gallery scraper, to create a intermediary images.js file.")
+parser.add_option("-g",             action="store",      dest="RUN_GALLERY",  default=False, help="Run the image gallery scraper, to create an intermediary images-GALLERY.js file.")
+parser.add_option("-q",             action="store_true", dest="RUN_QUOTES",   default=False, help="Run the quote parser, to create a intermediary quotes.js file.")
+parser.add_option("-t",             action="store_true", dest="RUN_TEMPLATE", default=False, help="Run the templating engine, to make an impulse.html file from the intermediary files.")
 (options, args) = parser.parse_args()
 
 if (options.RUN_ALL):
     quotes = parse_quotes_as_json()
-    images = get_image_info_as_json(defaults['flickrkey'], "152954026-72157680317429891")
+    images = get_all_galleries(defaults['flickrkey'], defaults['gallery_ids'])
     parse_template(quotes, images, defaults['greetingname'])
 
 if (options.RUN_QUOTES):
     parse_quotes_as_json()
 
 if (options.RUN_IMAGES):
-    get_image_info_as_json(defaults['flickrkey'], "152954026-72157680317429891")
+    get_all_galleries(defaults['flickrkey'], defaults['gallery_ids'])
+
+if (options.RUN_GALLERY):
+    get_image_info_as_json(defaults['flickrkey'], options.RUN_GALLERY)
 
 if (options.RUN_TEMPLATE):
     f = open("static/images.js", "r")
